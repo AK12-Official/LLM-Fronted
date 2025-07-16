d5366695b2049cf0cbe33d90dd5d043a4fae211a
AK12_Official <995929122@qq.com>
2025-07-16 21:20:00 +0800

    请求响应/拦截器

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,20 +1,20 @@
 //对 axios 进行二次封装：使用请求拦截器、响应拦截器
 import axios from "axios";
 
-//step 1：利用axios的create方法创建一个axios实例 （其他配置：基础路径、超时时间）
+//利用axios的create方法创建一个axios实例 （其他配置：基础路径、超时时间）
 const request = axios.create({
   //基础路径
   baseURL: '/api',//基础路径会携带/api
   timeout: 5000 // 超时时间的设置
 });
 
-//step 2:给axios实例添加请求与响应拦截器
+//给axios实例添加请求与响应拦截器
 request.interceptors.request.use((config) => {
   //返回配置对象
   return config
 });
 
-//step 3:响应拦截器
+//响应拦截器
 request.interceptors.response.use((response) => {
   //成功回调
 
